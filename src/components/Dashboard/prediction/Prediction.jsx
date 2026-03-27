488a72a9a5fe2822e0fcfdf786647b03f967fb4a
sanjidasefa <sanjidasefa9@gmail.com>
2026-03-27 23:09:00 +0600

    add external link

--- src/components/Dashboard/prediction/Prediction.jsx
+++ src/components/Dashboard/prediction/Prediction.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import { LayoutGrid, Globe, X, Activity } from 'lucide-react';
+import { LayoutGrid, Globe, X, Activity, ShieldCheck, ExternalLink } from 'lucide-react';
 import api from '../../../api/axios';
 
 const Prediction = () => {
